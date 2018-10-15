8b121bf9b3f0b3a9296fc5236d754e451d19627e
Nicolas Holanda de Menezes <nikolas.nhm14@gmail.com>
2018-10-15 17:20:32 -0300

    dec_p_n e ler matriz

--- ep1.c
+++ ep1.c
@@ -2,36 +2,103 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <math.h>
+#include <string.h>
 
-double decimal_p_bin(double dec){
-    double bin=0.0;
-    int parte_int = dec;
+
+void decimal_p_n(double dec, int base){
+    int resto = 0, i=0, parte_int = dec;
     dec-=parte_int;
-    int resto = 0;
-    int bits[15]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int i = 0;
+    char *bits = malloc((sizeof(dec)*8)+1);
     //PARTE INTEIRA
     while(parte_int >= 1){
-        resto = parte_int%2;
-        bits[i] = resto;
-        parte_int = parte_int/2;
+        int aux = (parte_int%base);
+        char c;
+        if(aux>9){
+            c = aux+7+'0';
+            bits[i] = c;
+        }
+        else{
+            bits[i] = aux+'0';
+        }
+        parte_int = parte_int/base;
         i++;
     }
-    for(i=14;i>=0;i--){
-        bin = bin + bits[i]*pow(10,i);
-        //printf("%d * 10**%d = %f\n",bits[i],i,bin);
+    for(--i;i>=0;i--){
+        printf("%c",bits[i]);
     }
+    printf(",");
     //PARTE FRACIONÁRIA
-    i=-1;
+    i=1;
     parte_int=dec;
-    while(dec>0 && i>-20){
-        dec*=2;
+    while(dec>0 && i<20){
+        dec*=base;
         parte_int=dec;
         dec -= parte_int;
-        bin += parte_int*pow(10,i);
-        i--;
+        if(parte_int>9){
+            printf("%c",parte_int+7+'0');
+        }
+        else{
+            printf("%d",parte_int);
+        }
+        i++;
     }
-    return bin;
+    free(bits);
+}
+
+int ler_matriz(){
+    FILE *arq;
+    arq = fopen("sl.txt","r");
+    if(arq==NULL){
+        printf("Algo errado aconteceu!\n");
+        return -1;
+    }
+    else{
+        char aux;
+        char *num=malloc(sizeof(char)*5);
+        strcpy(num," ");
+        int mult = 1;
+        int i=0,j=0,cont=0;
+        while(aux!='\n'){
+            aux=fgetc(arq);
+            num[cont]=aux;
+            cont++;
+        }
+        cont=0;
+        int tam = atoi(num);
+        int matriz[tam][tam+1];
+        strcpy(num," ");
+        while(1){
+            aux=fgetc(arq);
+            if(aux=='-'){
+                mult=-1;
+            }
+            else if(aux==' '){
+                matriz[i][j]=atoi(num)*mult;
+                strcpy(num," ");
+                mult=1;
+                cont=0;
+                j++;
+            }
+            else if(aux=='\n'){
+                matriz[i][j]=atoi(num)*mult;
+                printf("\n");
+                strcpy(num," ");
+                mult=1;
+                cont=0;
+                i++;
+                j=0;
+            }
+            else if(aux==EOF){
+                matriz[i][j]=atoi(num)*mult;
+                break;
+            }
+            else{
+                num[cont]=aux;
+                cont++;
+            }
+        }
+    }
+    fclose(arq);
 }
 
 void menu(){
@@ -53,18 +120,32 @@ void menu(){
                 printf("Digite um numero: ");
                 scanf("%f",&bin);
                 setbuf(stdin, NULL);
-                printf("%f em bin: %.20f\n",bin,decimal_p_bin(bin));
+                printf("Binario: ");
+                decimal_p_n(bin,2);
+                printf("\n");
+                printf("Octal: ");
+                decimal_p_n(bin,8);
+                printf("\n");
+                printf("Hexadecimal: ");
+                decimal_p_n(bin,16);
+                printf("\n");
+            break;
+            case 'S':
+                ler_matriz();
+            break;
+            case 'F':
+                system("cls");
+                printf("Ate logo!\n");
             break;
             default:
                 system("cls");
                 printf("Opcao invalida!");
+            break;
         }
     }
 }
 
 int main(){
-    //float bin = decimal_p_bin(221.0);
     menu();
-    //printf("\n %f",bin);
     return 0;
 }
